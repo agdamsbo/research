dfbfe78c2870eb911db2d0e72df8e7bc0db56e60
agdamsbo <43278243+agdamsbo@users.noreply.github.com>
2018-10-02 12:09:29 +0200

    Update cie_test.R

--- cie_test.R
+++ cie_test.R
@@ -1,4 +1,4 @@
-cie_test<-function(v1,v2=NULL,v3=NULL,y,string,data,cut=0.1){
+cie_glm_test<-function(v1,v2=NULL,v3=NULL,y,string,data,logistic=FALSE,cut=0.1){
 ## Calculating variables, that should be included for a change in estimate analysis. 
 ## v1-3 are possible locked variables, y is the outcome vector. 
 ## String defines variables to test, and is provided as vector of variable names. Use dput().
@@ -6,11 +6,11 @@ cie_test<-function(v1,v2=NULL,v3=NULL,y,string,data,cut=0.1){
   
   require(broom)
   
-  if (is.factor(y)){stop("Some kind of error message would be nice, but y should not be a factor!")}
-  
   d<-data
   x<-select(d,one_of(c(string)))
-  
+  if(logistic==FALSE){
+if (is.factor(y)){stop("Some kind of error message would be nice, but y should not be a factor!")}
+
 if (is.null(v2)&is.null(v3)){     
   
   e<-as.numeric(round(coef(lm(y~v1)),3))[1]
@@ -51,7 +51,7 @@ df<-cbind(df,t)
 
      df<-rbind(df,cbind(pred,b))
   }
-       t<-c(NA,ifelse(abs(e-as.numeric(-1,2]))>=(e*cut),"include","drop"))
+       t<-c(NA,ifelse(abs(e-as.numeric(df[-1,2]))>=(e*cut),"include","drop"))
 df<-cbind(df,t)  
        }
        
@@ -76,7 +76,78 @@ df<-cbind(df,t)
   }
        t<-c(NA,ifelse(abs(e-as.numeric(df[-1,2]))>=(e*cut),"include","drop"))
 df<-cbind(df,t)  
-}
+}}
+
+if(logistic==TRUE){
+
+if (!is.factor(y)){stop("Some kind of error message would be nice, but y should be a factor!")}
+
+if (is.null(v2)&is.null(v3)){     
+  
+  e<-as.numeric(round(exp(coef(glm(y~v1,family=binomial()))),3))[1]
+     
+       df<-data.frame(pred="base",b=e)
+  
+  for(i in 1:ncol(x)){
+
+     m<-glm(y~v1+x[,i],family=binomial())
+     
+     b<-as.numeric(round(exp(coef(m)),3))[1]
+  
+     v<-x[,i]
+     
+   pred<-paste(names(x)[i])
+
+     df<-rbind(df,cbind(pred,b))
+  }
+       t<-c(NA,ifelse(abs(e-as.numeric(df[-1,2]))>=(e*cut),"include","drop"))
+df<-cbind(df,t)  
+       }
+
+       if (!is.null(v2)&is.null(v3)){   
+         
+           e<-as.numeric(round(exp(coef(glm(y~v1+v2,family=binomial()))),3))[1]
+     
+       df<-data.frame(pred="base",b=e)
+       
+  for(i in 1:ncol(x)){
+
+     m<-glm(y~v1+v2+x[,i],family=binomial())
+     
+     b<-as.numeric(round(exp(coef(m)),3))[1]
+  
+     v<-x[,i]
+
+       pred<-paste(names(x)[i])
+
+     df<-rbind(df,cbind(pred,b))
+  }
+       t<-c(NA,ifelse(abs(e-as.numeric(df[-1,2]))>=(e*cut),"include","drop"))
+df<-cbind(df,t)  
+       }
+       
+       if (!is.null(v2)&!is.null(v3)){   
+         
+           e<-as.numeric(round(exp(coef(glm(y~v1+v2+v3,family=binomial()))),3))[1]
+     
+       df<-data.frame(pred="base",b=e)
+       
+  for(i in 1:ncol(x)){
+
+     m<-glm(y~v1+v2+v3+x[,i],family=binomial())
+     
+     b<-as.numeric(round(exp(coef(m)),3))[1]
+     
+     v<-x[,i]
+    
+       pred<-paste(names(x)[i])
+     
+     df<-rbind(df,cbind(pred,b))
+     
+  }
+       t<-c(NA,ifelse(abs(e-as.numeric(df[-1,2]))>=(e*cut),"include","drop"))
+df<-cbind(df,t)  
+}}
 
     return(df)
 }
