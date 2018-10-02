80775dfd3db058b00b4c789109e977b3145b4334
agdamsbo <43278243+agdamsbo@users.noreply.github.com>
2018-10-02 15:11:37 +0200

    Update rep_lm.R

--- rep_lm.R
+++ rep_lm.R
@@ -1,4 +1,4 @@
-rep_lm<-function(rep_glm<-function(y,v1,string,ci=FALSE,data,v2=NULL,v3=NULL){
+rep_lm<-function(y,v1,string,ci=FALSE,data,v2=NULL,v3=NULL){
 ## x is data.frame of predictors, y is vector of an aoutcome as a factor
 ## output is returned as coefficient, or if ci=TRUE as coefficient with 95 % CI.
 ## The confint() function is rather slow, causing the whole function to hang when including many predictors and calculating the ORs with CI.
@@ -7,25 +7,29 @@ rep_lm<-function(rep_glm<-function(y,v1,string,ci=FALSE,data,v2=NULL,v3=NULL){
   
   d<-data
   x<-select(d,one_of(c(string)))
+  m1<-length(coef(lm(y~v1)))
   
 if (is.factor(y)){stop("Some kind of error message would be nice, but y should not be a factor!")}
   
     if (ci==TRUE){
       
-             df<-data.frame(matrix(ncol = 3))
-  names(df)<-c("pred","co_ci","pv")
+             df<-data.frame(matrix(ncol = 4))
+  names(df)<-c("pred","co_ci","pv","t")
       
       for(i in 1:ncol(x)){
      m<-lm(y~v1+x[,i])
      
-     l<-suppressMessages(round(confint(m)[-1,1],2))
-     u<-suppressMessages(round(confint(m)[-1,2],2))
-     co<-round(coef(m)[-1],2)
+     l<-suppressMessages(round(confint(m)[-c(1:m1),1],2))
+     u<-suppressMessages(round(confint(m)[-c(1:m1),2],2))
+     co<-round(coef(m)[-c(1:m1)],2)
      
      co_ci<-paste0(co," (",l," to ",u,")")
      
-     pv<-round(tidy(m)$p.value[-1],3)
+     pv<-round(tidy(m)$p.value[-c(1:m1)],3)
      pv<-ifelse(pv<0.001,"<0.001",pv)
+        
+        t <- ifelse(pv<=0.1|pv=="<0.001","include","drop")
+        
      pv <- ifelse(pv<=0.05|pv=="<0.001",paste0("*",pv),
                   ifelse(pv>0.05&pv<=0.1,paste0(".",pv),pv))
 
@@ -38,22 +42,25 @@ if (is.factor(y)){stop("Some kind of error message would be nice, but y should n
        
        else {pred<-names(x)[i]}
       
-      df<-rbind(df,cbind(pred,co_ci,pv))
+      df<-rbind(df,cbind(pred,co_ci,pv,t))
       
     }}
     
   if (ci==FALSE){
      
-       df<-data.frame(matrix(ncol = 3))
-  names(df)<-c("pred","b","pv")
+       df<-data.frame(matrix(ncol = 4))
+  names(df)<-c("pred","b","pv","t")
      
   for(i in 1:ncol(x)){
      m<-lm(y~v1+x[,i])
      
-     b<-round(coef(m)[-1],3)
+     b<-round(coef(m)[-c(1:m1)],3)
      
-     pv<-round(tidy(m)$p.value[-1],3)
+     pv<-round(tidy(m)$p.value[-c(1:m1)],3)
      pv<-ifelse(pv<0.001,"<0.001",pv)
+    
+    t <- ifelse(pv<=0.1|pv=="<0.001","include","drop")
+    
      pv <- ifelse(pv<=0.05|pv=="<0.001",paste0("*",pv),
                   ifelse(pv>0.05&pv<=0.1,paste0(".",pv),pv))
      
